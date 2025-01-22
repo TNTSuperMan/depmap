b464078a68360804ce5ad558599d283c2313f60b
TNTSuperMan <tntsuperga@gmail.com>
2025-01-22 12:52:15 +0900

    cli版の改善

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,11 @@
+import { writeFileSync } from "fs";
 import import_map from "./module"
-if(process.argv.length != 3){
-    console.log("usage: import-map [entrypoint]");
+import { resolve } from "path";
+if(process.argv.length != 4){
+    console.log("usage: import-map [entrypoint] [outfile]");
 }else{
-    import_map(process.cwd(), process.argv[2])
+    const data = import_map(process.cwd(), process.argv[2]);
+    writeFileSync(resolve(process.cwd(), process.argv[3]),
+        JSON.stringify(Object.fromEntries(
+            data.keys().map(e=>[e, Array.from(data.get(e)??[])]))))
 }
